2032ef4e649abd11dae702d80e8f1a4617282239
Umberto Pasinetti <pasinetti96@gmail.com>
2023-10-18 10:27:58 +0200

    fix icon path

--- js/utils.js
+++ js/utils.js
@@ -93,28 +93,30 @@ export const getOriginalIcon = code =>
   `https://openweathermap.org/img/wn/${code}@2x.png`;
 
 // TODO: hour based img
+const baseUrl = 'https://egesiapres.github.io/weather-app';
+
 export const getCustomIcon = weather => {
   let icon;
 
   switch (weather) {
     case 'Clear':
-      icon = '/img/clear-day.svg';
+      icon = `${baseUrl}/img/clear-day.svg`;
       break;
 
     case 'Clouds':
-      icon = '/img/cloudy.svg';
+      icon = `${baseUrl}/img/cloudy.svg`;
       break;
 
     case 'Mist':
-      icon = '/img/mist.svg';
+      icon = `${baseUrl}/img/mist.svg`;
       break;
 
     case 'Rain':
-      icon = '/img/rain.svg';
+      icon = `${baseUrl}/img/rain.svg`;
       break;
 
     case 'Snow':
-      icon = '/img/snow.svg';
+      icon = `${baseUrl}/img/snow.svg`;
       break;
   }
 
@@ -125,79 +127,79 @@ export const getBftIcon = speed => {
   const bftScale = [
     {
       bft: 0,
-      icon: '/img/wind-beaufort-0.svg',
+      icon: `${baseUrl}/img/wind-beaufort-0.svg`,
       description: 'Calm',
       interval: '',
     },
     {
       bft: 1,
-      icon: '/img/wind-beaufort-1.svg',
+      icon: `${baseUrl}/img/wind-beaufort-1.svg`,
       description: 'Light air',
       interval: [1, 5],
     },
     {
       bft: 2,
-      icon: '/img/wind-beaufort-2.svg',
+      icon: `${baseUrl}/img/wind-beaufort-2.svg`,
       description: 'Light breeze',
       interval: [5, 11],
     },
     {
       bft: 3,
-      icon: '/img/wind-beaufort-3.svg',
+      icon: `${baseUrl}/img/wind-beaufort-3.svg`,
       description: 'Gentle breeze',
       interval: [11, 19],
     },
     {
       bft: 4,
-      icon: '/img/wind-beaufort-4.svg',
+      icon: `${baseUrl}/img/wind-beaufort-4.svg`,
       description: 'Moderate breeze',
       interval: [19, 28],
     },
     {
       bft: 5,
-      icon: '/img/wind-beaufort-5.svg',
+      icon: `${baseUrl}/img/wind-beaufort-5.svg`,
       description: 'Fresh breeze',
       interval: [28, 38],
     },
     {
       bft: 6,
-      icon: '/img/wind-beaufort-6.svg',
+      icon: `${baseUrl}/img/wind-beaufort-6.svg`,
       description: 'Strong breeze',
       interval: [38, 49],
     },
     {
       bft: 7,
-      icon: '/img/wind-beaufort-7.svg',
+      icon: `${baseUrl}/img/wind-beaufort-7.svg`,
       description: 'High wind',
       interval: [49, 61],
     },
     {
       bft: 8,
-      icon: '/img/wind-beaufort-8.svg',
+      icon: `${baseUrl}/img/wind-beaufort-8.svg`,
       description: 'Gale',
       interval: [61, 74],
     },
     {
       bft: 9,
-      icon: '/img/wind-beaufort-9.svg',
+      icon: `${baseUrl}/img/wind-beaufort-9.svg`,
       description: 'Strong gale',
       interval: [74, 88],
     },
     {
       bft: 10,
-      icon: '/img/wind-beaufort-10.svg',
+      icon: `${baseUrl}/img/wind-beaufort-10.svg`,
       description: 'Storm',
       interval: [88, 102],
     },
     {
       bft: 11,
-      icon: '/img/wind-beaufort-11.svg',
+      icon: `${baseUrl}/img/wind-beaufort-11.svg`,
       description: 'Violent storm',
       interval: [102, 118],
     },
     {
       bft: 12,
-      icon: '/img/wind-beaufort-12.svg',
+      icon: `${baseUrl}/img/wind-beaufort-12.svg`,
       description: 'Hurricane-force',
       interval: '',
     },
