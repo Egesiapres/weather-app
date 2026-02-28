42607e48e348903f2d1dce3f107bac860e8b88f8
Umberto Pasinetti <umberto.pasinetti@link-up.it>
2026-02-28 22:48:54 +0100

    moved constants in a dedicated file

---
+++ utils/constants.js
@@ -0,0 +1,6 @@
+const API_KEY = "d84df6a5359d1ca50cf0749743171b50";
+const baseOpenWeatherUrl = "https://api.openweathermap.org";
+const dataUrl = "/data/2.5";
+const geolocationUrl = "/geo/1.0";
+
+export { baseOpenWeatherUrl, dataUrl, geolocationUrl, API_KEY };
